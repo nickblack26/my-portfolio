da395cf9f70a29edb508b515a067361e9fe55be9
Nick Black <nicholas.black98@icloud.com>
2022-03-11 15:37:51 -0600

    added new meta image

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,16 @@ export default class MyDocument extends Document {
 			<Html>
 				<title>Nick Black | Front-End Developer</title>
 				<Head>
+					<meta
+						property='og:title'
+						content='Nick Black | Front-End Developer'
+						key='title'
+					/>
+					<meta
+						name='og::description'
+						content='Hey, my name is Nick Black, I am a 24 years old web developer. I am a dedicated web developer with 2+ years of experience collaborating with executive leaders to build responsive websites. I am also a hardworking creator with a passion for crafting exceptional, one-of-a-kind user experiences.'
+					/>
+					<meta content='/images/metaimage.png' property='og:image' />
 					<link
 						href='https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap'
 						rel='stylesheet'
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,11 +21,7 @@ const Home = () => {
 					name='og::description'
 					content='Hey, my name is Nick Black, I am a 24 years old web developer. I am a dedicated web developer with 2+ years of experience collaborating with executive leaders to build responsive websites. I am also a hardworking creator with a passion for crafting exceptional, one-of-a-kind user experiences.'
 				/>
-				<meta
-					property='og:image'
-					content='/metaimage.png'
-					key='ogimage'
-				/>
+				<meta content='/images/metaimage.png' property='og:image' />
 			</Head>
 			<Layout>
 				<Section grid>
