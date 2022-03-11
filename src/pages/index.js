aa2c6d344dbab46dc518878befa97483676a871f
Nick Black <nicholas.black98@icloud.com>
2022-03-11 14:52:41 -0600

    added a new page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,28 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Head from 'next/head';
 
 const Home = () => {
 	return (
-		<Layout>
-			<Section grid>
-				<Hero />
-				<BgAnimation />
-			</Section>
-			<Technologies />
-			<Timeline />
-			<Acomplishments />
-		</Layout>
+		<>
+			<Head>
+				<title>Nick Black | Front-End Developer</title>
+				<meta
+					name='description'
+					content='Hey, my name is Nick Black, I am a 24 years old web developer. I am a dedicated web developer with 2+ years of experience collaborating with executive leaders to build responsive websites. I am also a hardworking creator with a passion for crafting exceptional, one-of-a-kind user experiences.'
+				/>
+			</Head>
+			<Layout>
+				<Section grid>
+					<Hero />
+					<BgAnimation />
+				</Section>
+				<Technologies />
+				<Timeline />
+				<Acomplishments />
+			</Layout>
+		</>
 	);
 };
 
